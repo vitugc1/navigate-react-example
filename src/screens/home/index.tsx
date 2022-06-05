926602734372edbc1cba0ee0d4640aa3b860fbf3
vitugc1 <victorcarva13022002@gmail.com>
2022-06-05 18:07:29 -0300

    create resquest to api

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text } from "react-native";
+import { Button, FlatList, Text } from "react-native";
 import { Container } from "./styles";
+import { api } from '../../services/api';
+
+type DataProps = {
+  id: number;
+  title: string;
+}
 
 export const Home = () => {
+  const [data, setData] = useState<DataProps[]>()
   const navigation = useNavigation();
 
   const handleOpenScreen = () => {
     navigation.navigate('profile', {
-      user: 'Victor'
+      user: 'Rafael'
     })
   }
+
+  useEffect(() => {
+    const getAlldata = async () => {
+      const response = await api.get('/posts')
+      setData(response.data)
+    }
+    getAlldata()
+  }, [])
+  
 
   return (
     <Container>
@@ -19,6 +35,14 @@ export const Home = () => {
         onPress={handleOpenScreen}
       />
       <Text>Home</Text>
+
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+      />
     </Container>
   )
 }
